1b3a31e2e4c000a67d6678adb2579b5b3241c073
Prashantt <veedfrnd@gmail.com>
2022-01-09 16:17:06 +0530

    firebase fcm added and token generated and test mssg done...

---
+++ app/src/main/java/com/notificationtuts/app/MyApp.java
@@ -0,0 +1,35 @@
+package com.notificationtuts.app;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.installations.InstallationTokenResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MyApp extends Application {
+    public static final String TAG = "Prashant";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
+                        Log.d(TAG, token);
+                    }
+                });
+    }
+}
